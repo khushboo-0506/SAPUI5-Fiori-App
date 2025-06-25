cce2090ca98caaceee1689c82c3da3248579c364
Khushboo Rai <128734723+khushboo-0506@users.noreply.github.com>
2025-06-26 02:39:30 +0530

    initial commit

---
+++ webapp/controller/InvoiceList.controller.js
@@ -0,0 +1,40 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], (Controller, JSONModel, Filter, FilterOperator) => {
+	"use strict";
+
+	return Controller.extend("ui5.walkthrough.controller.InvoiceList", { 
+		onInit() {
+			const oViewModel = new JSONModel({
+				currency: "EUR"
+			});
+			this.getView().setModel(oViewModel, "view");
+		},
+
+		onFilterInvoices: function (oEvent) {
+			const aFilter = [];
+			const sQuery = oEvent.getParameter("query");
+			if (sQuery) {
+				aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+			}
+
+			const oList = this.byId("invoiceList");
+			const oBinding = oList.getBinding("items");
+			oBinding.filter(aFilter);
+		},
+
+		// ✅ FIXED: Added `oEvent` as parameter here
+		onPress: function (oEvent) {
+			const oItem = oEvent.getSource();
+			const oRouter = this.getOwnerComponent().getRouter();
+			oRouter.navTo("detail", {
+				invoicePath: window.encodeURIComponent(
+					oItem.getBindingContext("invoice").getPath().substring(1)
+				)
+			});
+		}
+	});
+});
